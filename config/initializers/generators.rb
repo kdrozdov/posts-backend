79b18609b4fbb2996ca8b11e89cbf9e9fd240eb0
Kirill Drozdov <kirill.dzv@gmail.com>
2016-07-15 02:21:06 +0500

    fix initializers

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,4 +1,4 @@
-app_config.generators do |g|
+Rails.application.config.generators do |g|
   g.test_framework :rspec
   g.fixture_replacement :factory_girl, dir: 'spec/factories'
 end
--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,4 +1,4 @@
-app_config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(*ENV.fetch('ALLOW_REQUESTS_FROM').split(','))
 
